2bc81320a6fb2fbba2de08d4bfc86080077990f1
YU-HSUAN TSENG <38046774+steven0323@users.noreply.github.com>
2021-01-03 19:33:22 +0800

    Add files via upload

--- a_mart_category.py
+++ a_mart_category.py
@@ -66,7 +66,10 @@ def get_category2(soup):
                 df['L1'].append(c1)
                 df['L2'].append(c2)
                 df['L3'].append(c3_)
-                df['L3 link'].append("https://shopping.friday.tw"+c3.find('a',href=True)['href'])
+                if "https" in c3.find('a',href=True)['href']:
+                    df['L3 link'].append(c3.find('a',href=True)['href'])
+                else:
+                    df['L3 link'].append("https://shopping.friday.tw"+c3.find('a',href=True)['href'])
                 
                         
                         
@@ -74,27 +77,28 @@ def get_category2(soup):
                 df['L1'].append(c1)
                 df['L2'].append(c2)
                 df['L3'].append(c3_)
-                df['L3 link'].append("None")
-
+                df['L3 link'].append("")
+    link_info = {'links':[]}
     for link in tqdm(df['L3 link']):
         
         if link=="None":
             pass
         else:
-            
-            
-            try:
-                
+            try:    
+                '''
                 opener = req.build_opener()
                 opener.addheaders = [{'User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHaTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}]
                 req.install_opener(opener)
                 response =  req.urlopen(link)
                 r = response.read().decode('utf-8')
-                soup = BeautifulSoup(r,'html.parser')
+                '''
+                browser.get(link)
+                soup = BeautifulSoup(browser.page_source,'html.parser')
                 p_links = soup.find('ul',class_="fourth_nave_box")
                 inner = p_links.find_all("li")
                 for j in inner:
                     product_links.append("https://shopping.friday.tw"+j.find('a',href=True)['href'])
+                    link_info['links'].append("https://shopping.friday.tw"+j.find('a',href=True)['href'])
             except:
                 time.sleep(5)
                 pass
@@ -102,7 +106,11 @@ def get_category2(soup):
     df = pd.DataFrame(df)
     df.index = np.arange(1,len(df)+1) 
     df.to_csv(dt+"_A_Mart_Category.csv",index=False)
+    df.to_excel(dt+"_A_Mart_Category.xlsx",index=False,engine='xlsxwriter')
     browser.quit()
+    link_info = pd.DataFrame(link_info)
+    link_info.to_csv(dt+"_amart_link.csv",index=False)
+    
     return df,product_links
 
 
@@ -113,59 +121,178 @@ def page_info(urls):
     '''
         Has potential issue with the multiple pages 
     '''
+    dt = date.today() 
+    dt = dt.strftime('%Y%m%d')  
     browser = webdriver.Chrome(executable_path="./chromedriver")
-    df = {"product":[],"price":[],'image url':[]} 
-    for url in tqdm(urls):
-        
-        
+    df = {"product":[],"product link":[],'image url':[],"product number":[],"price":[],"suggested price":[],"sales":[],'description':[],"specification":[],"detailed specification":[],"category 1":[],"category 2":[],"category 3":[],"category 4":[],"category 5":[]} 
+    for url in tqdm(urls['links']):
         try:
+            
             browser.get(url)
             time.sleep(0.5)
             soup = BeautifulSoup(browser.page_source,'html.parser')
             #tag = soup.find("ul",id_="prodlist").find_all("li")
-            tag = soup.find("div",class_="prodlist_box").find("ul",class_="content")
+
+            if soup.find("div",class_="prodlist_box").find("ul",class_="content"):
+                tag = soup.find("div",class_="prodlist_box").find("ul",class_="content")
         
-            for t in tag:
-            
-                try:
-                    df['product'].append(t.find('p',class_="product_name").text)
-                except:
-                    df['product'].append("None")
-                try:
-                    df['price'].append(t.find("p",class_="prod_price").text)
-                except:
-                    df['price'].append("None")
-                try:
-                    df['image url'].append(t.find("div",class_="prod_photo").find('img')['src'])
-                except:
-                    df['image url'].append("None")
-              
-            
-            
+                for t in tag:
+                
+                    df['product link'].append(url)
+                    df['product number'].append("")
+                    try:
+                        df['suggested price'].append(t.find("p",class_="prod_price").text)
+                    except:
+                        df['suggested price'].append("")
+                    df['sales'].append("")
+                    df['description'].append("")
+                    df['specification'].append("")
+                    df['detailed specification'].append("")
+                    df['category 1'].append("")
+                    df['category 2'].append("")
+                    df['category 3'].append("")
+                    df['category 4'].append("")
+                    df['category 5'].append("")
+
+                    try:
+                        df['product'].append(t.find('p',class_="product_name").text)
+                    except:
+                        df['product'].append("")
+                    try:
+                        df['price'].append(t.find("p",class_="prod_price").text)
+                    except:
+                        df['price'].append("")
+                    try:
+                        df['image url'].append(t.find("div",class_="prod_photo").find('img')['src'])
+                    except:
+                        df['image url'].append("")
+
+            elif soup.find("div",class_="prodlist_box").find("ul",class_="content array"):
+                tag = soup.find("div",class_="prodlist_box").find("ul",class_="content array")
+                prod = tag.find_all("li")
+                in_link = []
+                for l in prod:    
+                    in_link.append("https://shopping.friday.tw"+l.find('a',href=True)['href'])
+                for l in in_link:
+                    browser.get(l)
+                    soup = BeautifulSoup(browser.page_source,'html.parser')
+                    
+                    try:
+                        df['product'].append(soup.find('h1',class_="product_name").text)
+                    except:
+                        df['product'].append("")
+
+                    try:
+                        df['product link'].append(l)
+                    except:
+                        df['product link'].append("")
+                    
+                    try:
+                        df['image url'].append("https"+soup.find("div",class_="prodimg").find('img')['src'])
+                    except:
+                        df['image url'].append("")
+
+
+                    try:
+                        df['price'].append(soup.find("div",class_="attract_block").find("span",class_="price_txt").text)
+                    except:
+                        df['price'].append("")
+
+                    try:
+                        df['detailed specification'].append(soup.find("div",class_="content specification_box").text)
+                    except:
+                        df['detailed specification'].append("")
+
+                    try:
+                        df['product number'].append(soup.find("div",class_="product_id").find("span",class_="e3_pid").text)
+                    except:
+                        df['product number'].append("")
+
+                    try:
+                        df['suggested price'].append(soup.find("span",class_="pricing").text)
+                    except:
+                        df['suggested price'].append("")
+                    
+                    try:
+                        df['description'].append(soup.find("h3",class_="introduction").text)
+                    except:
+                        df['description'].append("")
+                    
+                    
+                    try:
+                        cat = soup.find("div",class_="path").find_all("a",href=True)
+                        df['category 1'].append(cat[1].text)
+                    except:
+                        df['category 1'].append("")
+
+                    try:
+                        df['category 2'].append(cat[2].text)
+                    except:
+                        df['category 2'].append("")
+
+                    try:
+                        df['category 3'].append(cat[3].text)
+                    except:
+                        df['category 3'].append("")
+
+                    try:
+                        df['category 4'].append(cat[4].text)
+                    except:
+                        df['category 4'].append("")
+
+                    try:
+                        df['category 5'].append(cat[5].text)
+                    except:
+                        df['category 5'].append("")
+
+                    df['specification'].append("")
+                    df['sales'].append("")
+                    
+            else:   
+                pass
             
         except:
-            print('exception occurs')
-            
+            print('exception occurs in :'+ url)
+            time.sleep(1.5)
             pass
+    '''
     browser.quit()
+    print(len(df['product']))
+    print(len(df['product link']))
+    print(len(df['product number']))
+    print(len(df['image url']))
+    print(len(df['price']))
+    print(len(df['suggested price']))
+    print(len(df['specification']))
+    print(len(df['detailed specification']))
+    print(len(df['description']))
+    print(len(df['category 1']))
+    print(len(df['category 2']))
+    print(len(df['category 3']))
+    print(len(df['category 4']))
+    print(len(df['category 5']))
+    print(len(df['sales']))
+    #print(df)
+    '''
     df = pd.DataFrame(df)
     df = df.drop_duplicates()
     df.index = np.arange(1,len(df)+1)
     df.to_csv(dt+"_A_Mart_info.csv",index=False)
+    df.to_excel(dt+"_A_Mart_info.xlsx",index=False,engine='xlsxwriter')
     return df
 
 
 def main():
 
-    dt = date.today() 
-    dt = dt.strftime('%Y%m%d')    
-  
-    url = "https://shopping.friday.tw/1/699.html"
-    soup = get_page(url)
-    df,links = get_category2(soup)
-    df = page_info(links)
-    print(df.shape)
-
+    
+    #url = "https://shopping.friday.tw/1/699.html"
+    #soup = get_page(url)
+    #df,links = get_category2(soup)
+    #link = pd.read_csv("20201228_amart_link.csv")
+    #df = page_info(link)
+    #print(df.shape)
+    df = pd.read_csv("20201228_A_Mart_info.csv")
+    df.to_excel("20201228_A_Mart_info.xlsx",index=False,engine='xlsxwriter')
 
 if __name__=="__main__":
     
